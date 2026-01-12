ebc7f8fb5066eed180f6245b6d1d0f5f70db350a
RaiyanSohel-byte <afnan19262@gmail.com>
2026-01-12 15:25:29 +0600

    mongoDB set up

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
+// MongoDB
+const client = new MongoClient(process.env.MONGO_URI);
+await client.connect();
+const db = client.db("bookwormDB");
+
 // server
 app.listen(port, () => {
   console.log("Server running on port", port);
