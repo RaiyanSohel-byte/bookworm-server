05449393ae802dc75c964c145175e01a34b43e90
RaiyanSohel-byte <afnan19262@gmail.com>
2026-01-12 15:26:09 +0600

    MongoClient import problem solved

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const { MongoClient } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
