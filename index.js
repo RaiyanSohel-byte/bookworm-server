5d4604e683d5748198b41df8cdb2cd2ba0f56038
RaiyanSohel-byte <afnan19262@gmail.com>
2026-01-12 15:20:53 +0600

    default middlewares added

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
