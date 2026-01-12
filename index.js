48ff111966ecc7732f9b2f3edfa7defb6414052c
RaiyanSohel-byte <afnan19262@gmail.com>
2026-01-12 15:22:44 +0600

    server set up

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const cookieParser = require("cookie-parser");
 require("dotenv").config();
 
 const app = express();
+const port = process.env.PORT || 5000;
+// middlewares
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+
+// server
+app.listen(port, () => {
+  console.log("Server running on port", port);
+});
